aeac263ca5f556a2f29cf55f276ca5845bbec142
amarbajric <amar.bajric78@gmail.com>
2016-11-11 19:38:25 +0100

    fixed major bug: -removed usage of CylinderGeometry and now using custom shape with paths -custom pie chart (shape) is now solid and not hollow anymore -added property for every segment where data can be stored

--- src/pieChart.js
+++ src/pieChart.js
@@ -52,7 +52,7 @@ animate();
 
 function init(){
     camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 1, 1000);
-    camera.position.z = 20;
+    camera.position.z = 15;
 
     controls = new THREE.TrackballControls( camera );
     controls.addEventListener('change', render);
@@ -64,19 +64,20 @@ function init(){
     renderer.setSize(window.innerWidth, window.innerHeight)
     document.body.appendChild(renderer.domElement);
 
+
     //ambient light which is for the whole scene
-    var ambientLight = new THREE.AmbientLight(0xffffff, 0.4);
+    var ambientLight = new THREE.AmbientLight(0xffffff, 0.7);
     ambientLight.castShadow = false;
     scene.add(ambientLight);
 
     //spot light which is illuminating the chart directly
-    var spotLight = new THREE.SpotLight(0xffffff, 0.6);
+    var spotLight = new THREE.SpotLight(0xffffff, 0.5);
     spotLight.castShadow = true;
     spotLight.position.set(0,50,0);
     scene.add(spotLight);
 
     //add grouped PieChart to scene
-    var groupedPieChart = create2DPieChart(data);
+    var groupedPieChart = create3DPieChart(data);
     //set the name of the object so it is easier to find in the scene again for the click function
     groupedPieChart.name = "groupedPieChart";
     scene.add(groupedPieChart);
@@ -87,6 +88,7 @@ function init(){
 
     //if window resizes
     window.addEventListener( 'resize', onWindowResize, false );
+
 }
 
 
@@ -104,9 +106,9 @@ function onDocumentMouseDown( event ) {
     var intersects = raycaster.intersectObjects(scene.getObjectByName("groupedPieChart", true).children); //search for our object by name which we declared before
 
     if(intersects[0] !== undefined && event.type === "mousedown") {
-        //print percentage of the clicked section + the name of the object assigned in the 'createXPieChart' function
+        //print percentage of the clicked section + the name of the object assigned in the 'create3DPieChart' function
         //intersects[0] because we want the first intersected object and every other object which may lies in the background is unnecessary
-        console.log(intersects[0].object.name, ":", parseFloat(((intersects[0].object.geometry.parameters.thetaLength * 100) / Math.PI) / 2).toFixed(5), "%");
+        console.log(intersects[0].object.name, "has a value of:", intersects[0].object.data, "%");
     }
     else if (intersects[0] !== undefined && event.type == "mousemove"){//if the event type is a mouse move (hover)
         //IMPLEMENT MOUSE HOVER ACTION HERE!
@@ -136,36 +138,62 @@ function onWindowResize() {
 
 
 
-
 /**
  * @input: JSON Object with data, scene object
  * @output: one ore more meshes which are then added to the scene (void)
  */
-function create2DPieChart(jsonData) {
+function create3DPieChart(jsonData) {
     //Group together all pieces
     var pieChart = new THREE.Group();
 
     //variable holds last position of the inserted segment of the pie
-    var lastThetaStart = 0;
+    var lastThetaStart = 0.0;
 
     //iterate over the jsonData and create for every data a new pie segment
     //data = one object in the json which holds the props "amount","percent" in this case.
     for (var data in jsonData){
-        //Cylinder Geometry: (radiusTop, radiusBottom, height, radialSegments, heightSegments, openEnded, thetaStart, thetaLength)
-        var segment = new THREE.Mesh(new THREE.CylinderGeometry(5,5,1.5,50,50,false,lastThetaStart,(Math.PI*2)*(jsonData[data].percent/100)),
-            new THREE.MeshPhongMaterial({
-                ambient: 0x808080,
-                color: Math.random() * 0xffffff
-            }));
+        var segment = createSegment(3,lastThetaStart, lastThetaStart + THREE.Math.degToRad(jsonData[data].percent*3.6));
 
         //assign the object the name from the description of the JSON
         segment.name = jsonData[data].description;
 
+        //define a new property for the segment to store the percent associated with it.
+        //source: https://developer.mozilla.org/de/docs/Web/JavaScript/Reference/Global_Objects/Object/defineProperty
+        Object.defineProperty(segment, 'data', {
+            value: parseFloat(jsonData[data].percent).toFixed(3)
+        });
+
         //set the lastThetaStart to the length of the last segment, in order to not overlap segments
-        lastThetaStart = lastThetaStart + (Math.PI*2)*(jsonData[data].percent/100);
+        lastThetaStart = lastThetaStart + THREE.Math.degToRad(jsonData[data].percent*3.6);
 
         //add new piece to the grouped pieChart
         pieChart.add(segment);
     }
     return pieChart;
+}
+
+
+
+function createSegment(radius, angleStart, angleEnd) {
+    var extrudeOptions = {
+        curveSegments: 32,
+        steps: 1,
+        amount: 1,
+        bevelEnabled: false,
+    };
+
+    var shape = new THREE.Shape();
+    shape.moveTo(0, 0);
+    shape.absarc(0, 0, radius, angleStart, angleEnd, false);//false to not go clockwise (otherwise it will fail)
+    shape.lineTo(0, 0);
+    var segmentGeom = new THREE.ExtrudeGeometry(shape,extrudeOptions);
+    var segmentMat = new THREE.MeshPhongMaterial({
+        color: Math.random() * 0xffffff,
+        shading: THREE.SmoothShading,
+        specular: 0xffffff,
+        shininess: 3,
+    });
+    var segment = new THREE.Mesh(segmentGeom, segmentMat);
+
+    return segment;
 }
