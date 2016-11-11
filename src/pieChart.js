0ec94d920a230443febe091b7d6ffc34d0ea7891
amarbajric <amar.bajric78@gmail.com>
2016-11-11 11:40:29 +0100

    added: -double sided material for circle geometry -random coloring for pie chart (removed color from json)

--- src/pieChart.js
+++ src/pieChart.js
@@ -13,33 +13,27 @@ import * as THREE from "three/build/three";
 var data = {
     "age_0-10": {
         "amount": 50123,
-        "percent": 17.901071428571428571428571428571,
-        "pieChartColor": "#9205FA"
+        "percent": 17.901071428571428571428571428571
     },
     "age_11-20": {
         "amount": 55000,
-        "percent": 19.642857142857142857142857142857,
-        "pieChartColor": "#17FA05"
+        "percent": 19.642857142857142857142857142857
     },
     "age_21-35": {
         "amount": 75000,
-        "percent": 26.785714285714285714285714285714,
-        "pieChartColor": "#05E4FA"
+        "percent": 26.785714285714285714285714285714
     },
     "age_36-50": {
         "amount": 41000,
-        "percent": 14.642857142857142857142857142857,
-        "pieChartColor": "#FA0535"
+        "percent": 14.642857142857142857142857142857
     },
     "age_51-65": {
         "amount": 24236,
-        "percent": 8.6557142857142857142857142857143,
-        "pieChartColor": "#FAC305"
+        "percent": 8.6557142857142857142857142857143
     },
     "age_66-75": {
         "amount": 34641,
-        "percent": 12.371785714285714285714285714286,
-        "pieChartColor": "#47470A"
+        "percent": 12.371785714285714285714285714286
     }
 }
 
@@ -139,7 +133,8 @@ function create2DPieChart(jsonData) {
     //data = one object in the json which holds the props "amount","percent" and "pieChartColor" in this case.
     for (var data in jsonData){
         var newMesh = new THREE.Mesh(new THREE.CircleGeometry(5,30,lastThetaStart,(Math.PI*2)*(jsonData[data].percent/100)),new THREE.MeshLambertMaterial({
-            color: jsonData[data].pieChartColor,
+            color: Math.random() * 0xffffff,
+            side: THREE.DoubleSide
         }));
 
         //set the lastThetaStart to the length of the last segment, in order to not overlap segments
