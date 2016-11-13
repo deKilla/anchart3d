19ea00507a3d4d9a9b72fff2a53e732bc458a796
deKilla <admin@dekilla.at>
2016-11-13 23:43:34 +0100

    file reader demo

--- src/jsonPrep.js
+++ src/jsonPrep.js
@@ -2,51 +2,40 @@
  * Created by Michael on 13.11.2016.
  */
 
-var JSON = { 
-    "data1": {
-        "values": [
-            {"value1": [{
-                "name":"Value1",
-            	"value":200
-                }]
-            },
-            {"value2": [{
-                "name":"Value2",
-            	"value":5
-                }]
+//geht
+//console.log(JSON.data1.values.length);
+//geht auch
+//console.log(JSON.data2.values[0].value1[0].value);
+
+// TODO
+// check if all data has the same amount of values (2 as of now)
+
+// TODO
+// sum up all values (alle value1s = 1st sum, all value2s = 2nd sum) of each data
+// in this example we need sumV1 = 800 // sumV2 == 12
+
+function getJsonText(file)
+{
+    var rawFile = new XMLHttpRequest();
+    rawFile.open("GET", file, false);
+    rawFile.onreadystatechange = function ()
+    {
+        if(rawFile.readyState === 4)
+        {
+            if(rawFile.status === 200 || rawFile.status == 0)
+            {
+                allText = rawFile.responseText;
             }
-        ]
-    },
-    "data2": {
-        "values": [
-            {"value1": [{
-                "name":"Value1",
-            	"value":500
-                }]
-            },
-            {"value2": [{
-                "name":"Value2",
-            	"value":4
-                }]
-            }
-        ]
-    },
-    "data3": {
-        "values": [
-            {"value1": [{
-                "name":"Value1",
-            	"value":100
-                }]
-            },
-            {"value2": [{
-                "name":"Value2",
-            	"value":3
-                }]
-            }
-        ]
+        }
     }
-};
+    rawFile.send(null);
+    return allText;
+}
 
-console.log(JSON.data1.values.length);
-console.log(JSON.data2.values[0].value1[0].value);
+let json = JSON.parse(getJsonText("../src/data.json"));
 
+console.log(json[0]);
+
+for (var data in json[0]) {
+	alert(data);
+}
