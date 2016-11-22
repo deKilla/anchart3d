0a20c9da9530058922412faaac7a9c41fa57716b
deKilla <admin@dekilla.at>
2016-11-22 21:04:34 +0100

    small legend adjustments

--- src/utils/legend.js
+++ src/utils/legend.js
@@ -13,20 +13,18 @@ class Legend {
 
     generateLegend(map = this.map) {
         map.forEach(function createHTML(value, key, map) {
-            let colorElem = document.createElement('span');
-            console.log(value);
-            colorElem.setAttribute("class", "color-box legend-block");
+            
+            let containerElem = document.createElement("li");
+
+            let colorElem = document.createElement("span");
+            colorElem.setAttribute("class", "color-box");
             colorElem.setAttribute("style", "background-color:#" + value + "");
 
-            let nameElem = document.createElement('li');
-            nameElem.setAttribute("class", "legend-block");
+            let nameElem = document.createElement("i");
             nameElem.textContent = key;
-
-
-            document.getElementById('legend')
-                .appendChild(colorElem)
-                .appendChild(nameElem);
-
+            
+            document.getElementById('legend').appendChild(containerElem).appendChild(colorElem);
+            document.getElementById('legend').appendChild(containerElem).appendChild(nameElem);
 
         })
 
